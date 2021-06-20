47826add65043f32a5bd74e7a0a70deecf0b419d
smandyscom <smandyscom@gmail.com>
2021-06-20 11:34:20 +0800

    reloacte variables

--- try_comm_to_external_sensors/switcher.py
+++ try_comm_to_external_sensors/switcher.py
@@ -2,28 +2,29 @@ import importlib
 from importlib.util import find_spec
 from random import random
 
-dummy_io_trigger = bool()
-dummy_io_acknowledge = bool()
-dummy_io_pos_index = int(-1)
-
-dummy_pos_current_mach = [float()]*3 # x,y,theta, from CNC
-dummy_pos_comp_mach = [float()]*3 # to CNC
-
 class Swither(object):
     def __init__(self, *args):
         super(Swither, self).__init__(*args)
+        
+        self.dummy_io_trigger = bool()
+        self.dummy_io_acknowledge = bool()
+        self.dummy_io_pos_index = int(-1)
+        
+        self.dummy_pos_current_mach = [float()]*3 # x,y,theta, from CNC
+        self.dummy_pos_comp_mach = [float()]*3 # to CNC
 
-        global dummy_io_trigger
-        global dummy_io_acknowledge
-        global dummy_io_pos_index
 
         #interfaces 
-        self.read_trigger = lambda : dummy_io_trigger
-        self.read_pos_index = lambda : dummy_io_pos_index
-        self.write_acknowledge = lambda x=False: (dummy_io_acknowledge:=x)[0]
+        self.read_trigger = lambda : self.dummy_io_trigger
+        self.read_pos_index = lambda : self.dummy_io_pos_index
+        self.write_acknowledge = lambda x: print(x) 
+        """ lambda x=False: (
+            self.dummy_io_acknowledge := '5',
+            )[-1] """
 
-        self.read_pos_current_mach = lambda : dummy_pos_current_mach
-        self.write_pos_comp_mach = lambda pos: (dummy_pos_comp_mach:=pos)[0]
+        self.read_pos_current_mach = lambda : self.dummy_pos_current_mach
+        self.write_pos_comp_mach = lambda x:print(x)
+        """ lambda pos: (self.dummy_pos_comp_mach:=pos)[0] """
 
         is_hal_exists = find_spec('hal') is not None
         is_linuxcnc_exists = find_spec('linuxcnc') is not None
@@ -35,10 +36,7 @@ class Swither(object):
         pass
     
     def dice_values(self):
-        global dummy_pos_current_mach
-
-        for pos in dummy_pos_current_mach:
+        for pos in self.dummy_pos_current_mach:
             pos = random()*1000
-
         pass
     pass
